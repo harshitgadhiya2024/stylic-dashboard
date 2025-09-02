3bde0f6fb40df1fe639694de8b505e92c61ceab4
harshitgadhiya2024 <155363296+harshitgadhiya2024@users.noreply.github.com>
2025-09-02 10:42:40 +0530

    changes

--- operations/ai_photoshoot_generation.py
+++ operations/ai_photoshoot_generation.py
@@ -475,6 +475,8 @@ def generate_photoshoot_background_task(garment_mapping_dict, photoshoot_id, upp
         parts = []
         if upper_garment_image:
             exten = upper_garment_image.split(".")[-1]
+            if exten in ["jpg", "jpeg"]:
+                exten = "jpeg"
             upper_garment_path = f"static/photoshoots_folders/{photoshoot_id}/{upper_garment_image}"
             with open(upper_garment_path, "rb") as f:
                 upper_image_data = f.read()
@@ -486,6 +488,8 @@ def generate_photoshoot_background_task(garment_mapping_dict, photoshoot_id, upp
 
         if below_garment_image:
             exten = below_garment_image.split(".")[-1]
+            if exten in ["jpg", "jpeg"]:
+                exten = "jpeg"
             lower_garment_path = f"static/photoshoots_folders/{photoshoot_id}/{below_garment_image}"
             with open(lower_garment_path, "rb") as f:
                 lower_image_data = f.read()
